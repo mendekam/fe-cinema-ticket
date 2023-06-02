54057184e5c1fa1847b4807b79d84657681acf02
Abid Bilal <abiddddbilal@gmail.com>
2023-06-03 02:21:08 +0700

    register

--- src/config/api.js
+++ src/config/api.js
@@ -2,5 +2,5 @@ const BASE_URL = 'https://springbootgcpcr-a6bnbhk5xa-as.a.run.app/'
 
 export default {
     apiLogin : BASE_URL + 'api/auth/login',
-    apiRegister : BASE_URL + 'api/auth/register',
+    apiRegis : BASE_URL + 'api/auth/register',
 }
